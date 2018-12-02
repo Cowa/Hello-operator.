b0f1399eb36f15bda38421a8d2e0bb6d2f2799fb
Brice Thomas <brice.p.thomas@gmail.com>
2018-12-02 22:21:46 +0100

    Improving

--- src/menu.gd
+++ src/menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+func _ready():
+	$AnimationPlayer.play("entrance")
+	yield($AnimationPlayer, "animation_finished")
+	$AnimationPlayer.play("ringing")
+
 func _on_Button_pressed():
 	$AnimationPlayer.play("leave")
 	yield($AnimationPlayer, "animation_finished")
